caf46049129f5d4e812f02f3356b323614025e11
devtud <devtud@gmail.com>
2021-01-09 13:55:59 +0200

    typing-extensions is not dev dependency

--- noxfile.py
+++ noxfile.py
@@ -8,12 +8,10 @@ def tests(session: Session) -> None:
     session.run("pip", "--version")
     session.run("pip", "install", "pip==20.2.4")
     session.run("poetry", "lock")
-    session.run("echo", "'after lock'", external=True)
     session.install(".")
     session.install("docker")
 
     if session.python in ["3.6", "3.7"]:
-        session.install("typing-extensions")
         session.install("aiounittest")
 
     session.run("python", "-m", "unittest")
