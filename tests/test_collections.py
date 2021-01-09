cfdae21665c7f6cf0085f45522d1ce3bb22f5897
devtud <devtud@gmail.com>
2021-01-09 15:42:05 +0200

    Keep 1 collectons test file

--- tests/test_collections.py
+++ tests/test_collections.py
@@ -7,6 +7,19 @@ from tests.docker_ import DockerTestCase
 
 
 class TestCollections(DockerTestCase):
+    async def test_collection_get_not_exists(self):
+        collection = await self.client.collections["fruits"].retrieve()
+        self.assertIsNone(collection)
+
+    async def test_collection_get(self):
+        created_col = await self.client.collections.create(collection_schema)
+        self.assertIn("name", created_col)
+        self.assertIn("fields", created_col)
+
+        collection = await self.client.collections["fruits"].retrieve()
+
+        self.assertEqual("fruits", collection["name"])
+
     async def test_collections_get_no_collections(self):
         collections = await self.client.collections.retrieve()
         self.assertEqual(0, len(collections))
